f97cafd326c667f744122cfc30309a80ec39d3e5
Himanshu Shekhar <himanshu@himanshu>
2016-04-03 04:04:55 +0530

    added exception handling

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-document.getElementById('mw-page-base').remove(); 
-document.getElementById('mw-head-base').remove(); 
-document.getElementById('mw-indicators').remove(); 
-document.getElementById('mw-siteNotice').remove(); 
-document.getElementById('mw-head').remove(); 
-document.getElementById('mw-siteSub').remove(); 
-document.getElementById('mw-contentSub').remove(); 
-document.getElementById('mw-jump-to-nav').remove();
-document.getElementById('mw-left-navigation').remove(); 
-document.getElementById('p-namespaces').remove(); 
-document.getElementById('mw-page-base').remove();  
-document.getElementById('mw-page-base').remove(); 
-document.getElementById('mw-page-base').remove();
-document.getElementById('mw-page-base').remove();
-document.getElementsByClassName('vectorTabs')[0].remove();
-document.getElementById('mw-page-base').remove();
+try {
+	document.getElementById('mw-page-base').remove(); 
+	document.getElementById('mw-head-base').remove(); 
+	document.getElementById('mw-indicators').remove(); 
+	document.getElementById('mw-siteNotice').remove(); 
+	document.getElementById('mw-head').remove(); 
+	document.getElementById('mw-siteSub').remove(); 
+	document.getElementById('mw-contentSub').remove(); 
+	document.getElementById('mw-jump-to-nav').remove();
+	document.getElementById('mw-left-navigation').remove(); 
+	document.getElementById('p-namespaces').remove(); 
+	document.getElementById('mw-page-base').remove();  
+	document.getElementById('mw-page-base').remove(); 
+	document.getElementById('mw-page-base').remove();
+	document.getElementById('mw-page-base').remove();
+	document.getElementsByClassName('vectorTabs')[0].remove();
+	document.getElementById('mw-page-base').remove();
+}
+catch (err) {}
