a65cd32c584b6f1ee50f4490c46a37c0c8ef5e70
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-04-03 04:46:47 +0530

    removed duplicates

--- script.js
+++ script.js
@@ -8,10 +8,6 @@ try { document.getElementById('mw-contentSub').remove(); } catch(err) {}
 try { document.getElementById('mw-jump-to-nav').remove(); } catch(err) {}
 try { document.getElementById('mw-left-navigation').remove(); } catch(err) {}
 try { document.getElementById('p-namespaces').remove(); } catch(err) {}
-try { document.getElementById('mw-page-base').remove(); } catch(err) {}
-try { document.getElementById('mw-page-base').remove(); } catch(err) {}
-try { document.getElementById('mw-page-base').remove(); } catch(err) {}
-try { document.getElementById('mw-page-base').remove(); } catch(err) {}
 try { document.getElementsByClassName('vectorTabs')[0].remove(); } catch(err) {}
 try { document.getElementById('mw-page-base').remove(); } catch(err) {}
 
