8a0497c09a8ad8238033fdbd055a3c01a501609f
Himanshu Shekhar <himanshu@himanshu>
2016-04-03 04:01:20 +0530

    Added few scripts to clean up the page

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('mw-page-base').remove(); 
+document.getElementById('mw-head-base').remove(); 
+document.getElementById('mw-indicators').remove(); 
+document.getElementById('mw-siteNotice').remove(); 
+document.getElementById('mw-head').remove(); 
+document.getElementById('mw-siteSub').remove(); 
+document.getElementById('mw-contentSub').remove(); 
+document.getElementById('mw-jump-to-nav').remove();
+document.getElementById('mw-left-navigation').remove(); 
+document.getElementById('p-namespaces').remove(); 
+document.getElementById('mw-page-base').remove();  
+document.getElementById('mw-page-base').remove(); 
+
